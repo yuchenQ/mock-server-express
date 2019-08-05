598b596406d8acb2118a3a8893aa3365eb00f43d
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2019-08-05 18:25:00 +1000

    delete comments

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 /** @format */
-// 第三方模块
 import bodyParser from 'body-parser';
 import express from 'express';
-import { NextFunction, Request, Response } from 'express'; // express 申明文件定义的类型
+import { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
-// 处理 post 请求的请求体，限制大小最多为 20 兆
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 app.use(bodyParser.json({ limit: '20mb' }));
 
-// error handler
 app.use(function(err: Error, req: Request, res: Response, next: NextFunction): void {
   res.sendStatus(500);
 
